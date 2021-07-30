4fd863071694902fc2920b249b28bdded808846d
Rudy Saavedra <rudysa2@gmail.com>
2021-07-30 01:25:01 -0400

    php setup

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>Welcome to the home page!</p>
+</div>
+
+<?=template_footer()?>
+
+<!-- <!DOCTYPE html>
+<html>
+
+<head>
+    <title>VIN CRUD OPERATIONS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="row justify-content-center">
+    <form action="" method="POST">
+        <div class="form-group">
+            <label>VIN</label>
+            <input type="text" name="VIN" class="form-control" value="Enter your VIN">
+        </div>
+        <div class="form-group">
+            <button type="submit" name="save">Save</button>
+        </div>
+    </form>
+</div>
+</body> -->
