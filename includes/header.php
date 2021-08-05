0b08e104463e5e7189ffb97b489fe9d8a800fa45
Rudy Saavedra <rudysa2@gmail.com>
2021-08-04 22:31:42 -0400

    analytics again

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,13 @@ session_start();
 <html>
 
 <head>
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WRKJH3Q');</script>
+<!-- End Google Tag Manager -->
     <meta charset="UTF-8">
     <title>Rudy's VIN Decoder</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
