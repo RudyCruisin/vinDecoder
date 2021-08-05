20a7dd2eb2bd2ced5cf297d843f5c4e855422429
Rudy Saavedra <rudysa2@gmail.com>
2021-08-04 23:28:09 -0400

    analytics removed, buggy

--- includes/header.php
+++ includes/header.php
@@ -1,32 +1,12 @@
 <?php
 require_once 'includes/database.php';
-session_start();
-?>
-<html>
-
+session_start();?><html>
 <head>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-R2RB6S0D5Y"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-R2RB6S0D5Y');
-</script>
-    <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WRKJH3Q');</script>
-<!-- End Google Tag Manager -->
     <meta charset="UTF-8">
     <title>Rudy's VIN Decoder</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
 </head>
-
 <body>
     <header>
         <nav class="navbar navbar-dark justify-content-center" style="background-color:#242428;">
@@ -39,5 +19,4 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 </li>
             </ul>
         </nav>
-
     </header>
